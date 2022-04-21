fe2f78cf0214cdc645a4851424b1099c9dde1416
Carr99 <andre@carrblad.se>
2022-04-21 20:25:07 +0200

    login now fully works

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -11,18 +11,46 @@ function login() {
       </br>
       <input id="passwordInput" class="loginInput" type="password" required></input>
       </br>
+      <p id="errorText" style="color:red" hidden>Error, Try again!</p>
       <button id="loginBtn">Log in</button><a id="signupBtn">Sign Up</a>
       </article>
       </div>
     `
   document.querySelector("#loginBtn").addEventListener("click", function () {
-    alert('buttonClicked')
+    loginClicked()
   })
   document.querySelector("#signupBtn").addEventListener("click", function () {
     alert('buttonClicked')
   })
 }
 
-function loginClicked() {
+async function loginClicked() {
+  let email = document.getElementById('emailInput').value
+  let password = document.getElementById('passwordInput').value
+  data = { 'email': email, 'password': password }
 
+  let result
+  try {
+    result = await (await fetch('http://localhost:3002/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })).json()
+  }
+  catch (ignore) { }
+
+  if (!result) {
+    document.getElementById('errorText').hidden = false
+    return
+  } else {
+    if (result.email !== email) {
+      document.getElementById('errorText').hidden = false
+      return
+    }
+  }
+
+  document.getElementById('errorText').hidden = true
+  //Redirect to new page
+  history.pushState(null, null, '/quizzes')
+  router()
 }
