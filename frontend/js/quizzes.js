55e1cb2f4e316d313fc743a882cebc9c94383d36
WallMartin94 <57918288+WallMartin94@users.noreply.github.com>
2022-05-10 13:39:10 +0200

    Start quiz
    One can now start a selected quiz

--- frontend/js/quizzes.js
+++ frontend/js/quizzes.js
@@ -1,6 +1,9 @@
 
 let quiz =''
 let quizzes =[]
+
+
+
 async function getQuizzes() {
   let result = {}
   try {
@@ -14,25 +17,37 @@ async function getQuizzes() {
   quizzes=result['quizzes']
   console.log(result)
 }
-
-
-
-
-
-
-
-
 function initQuizzes(){
 
   console.log("Intializing...")
   getQuizzes()
+  
+    document.querySelector('.quizBox').addEventListener('click', function(e) {  
+                                
+       
+      e.preventDefault()  
+      let aTag = e.target.closest('a');
+      let aTags = document.querySelectorAll('a');
+  
+      for (let currentTag of aTags) {
+      currentTag.classList.remove('selected');
+       
+  }
+      aTag.className= 'selected'; 
 
+      
+    })
+   
+   
+    
 document.querySelector("#filterButton").addEventListener("click", function () {
 filterFunction(document.querySelector("#quizSelect").value)
 console.log("You filtered...")
-console.log(document.querySelector("#quizSelect").value)
+console.log(document.querySelector("#quizSelect").value) })
 
-})
+
+
+
 document.querySelector(".highscoreButton").addEventListener("click", function () {
 
 //Code for moving to the highscorepage
@@ -40,15 +55,36 @@ document.querySelector(".highscoreButton").addEventListener("click", function ()
 })
 
 document.querySelector(".startButton").addEventListener("click", function () {
-
-let category = document.querySelector(".quizBox").getText()
-
-//Get quiz id and pass it to the question page and load it.
+  
 
 
-})
+selectedQuiz=document.querySelector(".selected").textContent
 
+if(!selectedQuiz==null){
+  history.pushState(null, null, '/question')
+  router()
+}else{
+alert("Please select a quiz before you try to start!")
 }
+
+
+
+
+
+  //Get quiz id and pass it to the question page and load it.
+  
+  
+  })
+}
+
+
+
+
+
+
+
+
+
 
 function filterFunction(category){
 console.log("In filter function...")
@@ -58,17 +94,28 @@ for (const quiz of quizzes) {
   
 if(category=="All"){
 
-  temporaryHtml+=`<li id=${quiz["name"]}>${quiz["name"]}</li>`}
+  temporaryHtml+=`<a href="javascript:void(0)" id=${quiz["name"]}>${quiz["name"]}</a>`}
 
 
 
 
 if(quiz['category']==category){
  
-temporaryHtml+=`<li id=${quiz["name"]}>${quiz["name"]}</li>`}
+temporaryHtml+=`<a href="javascript:void(0)" id=${quiz["name"]}>${quiz["name"]}</a>`}
 
 }
 console.log(temporaryHtml)
 document.querySelector(".quizBox").innerHTML=temporaryHtml
 
 }
+
+function getCurrentQuiz(){
+
+  
+
+  console.log("The current quiz is: ", currentQuiz)
+
+
+}
+
+
