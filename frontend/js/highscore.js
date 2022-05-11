fe31049362b712e83b47dcd66ed653c7954cc9a8
WallMartin94 <57918288+WallMartin94@users.noreply.github.com>
2022-05-11 11:02:37 +0200

    Sorted highscore function
    Adjusted function to be more correct with the new route. Removed unnecessary css parts.

--- frontend/js/highscore.js
+++ frontend/js/highscore.js
@@ -1,24 +1,31 @@
 let highscores
-let users
+
 async function initScore(quizId) {
 
   await getScore(quizId)
-  await getUsers()
   console.log(highscores)
   tempHtml = ``
-  for (const highscore of highscores) {
-    
-    for (const user of users) {
-      if (highscore['user'] == user['id']) {
-        console.log(user)
-        tempHtml += `<h2  id=${user["id"]}>${user["data"]["username"]}: ${highscore["score"]}</h2>`
-      }
-    }
-
+  for (let highscore of highscores) {
+    tempHtml +=` <h2>${highscore["user"]}: ${highscore["score"]}</h2>`
   }
-  document.querySelector("#scoreBox").innerHTML = tempHtml
 
+  document.querySelector("#scoreBox").innerHTML = tempHtml
 }
+
+async function getScore(quizId) {
+  let result = {}
+  try {
+    result = await (await fetch(`/api/highscores/${quizId}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    })).json()
+  } catch (error) {
+    console.error(error);
+  }
+  highscores = result['scores']
+  console.log(result)
+} 
+
 
 async function getScore(quizId) {
   let result = {}
